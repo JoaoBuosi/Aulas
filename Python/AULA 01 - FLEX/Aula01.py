8b19eec214d169f6f2437a8f2e39fa2d99873d5c
João Buosi <joaovbuosi@gmail.com>
2025-09-21 16:52:44 -0300

    Add files via upload

---
+++ Python/AULA 01 - FLEX/Aula01.py
@@ -0,0 +1,51 @@
+#Exercicio 1
+animal = input("Qual é o nome do seu animal de estimação? ")
+idade = input("Quantos anos ele tem? ")
+print(f"O {animal} tem {idade} anos!")
+
+#Exercicio 2
+time = input("Qual é o seu time de futebol favorito? ")
+print(f"O seu time de futebol favorito é {time}!!!")
+
+#Exercicio 3
+heroi = input("Qual é o seu super-herói favorito? ")
+print(f"O seu herói favorito é {heroi}!")
+
+#Exercicio 4
+estacao = input("Qual é a sua estação do ano favorita (verão, outono, inverno ou primavera)? ")
+print(f"A estação que você mais gosta é {estacao}!")
+
+#Exercicio 5
+palavra1 = input("Digite a primeira palavra: ")
+palavra2 = input("Digite a segunda palavra: ")
+print("Nome criativo:", palavra1 + palavra2)
+
+#Exercicio 6
+meta = input("Digite uma meta sua para este ano: ")
+print(f"Você consegue alcançar sua meta de {meta}! Continue firme!")
+
+#Exercicio 7
+livro = input("Digite o título de um livro que você gosta: ")
+autor = input("Digite o nome do autor desse livro: ")
+print(f"O livro '{livro}' foi escrito por {autor}.")
+
+#Exercicio 8
+dia = input("Qual é o dia da semana que você mais gosta? ")
+print(f"Ah, então você ama {dia}! Esse dia é realmente especial.")
+
+#Exercicio 9
+nome1 = input("Digite o nome da primeira pessoa: ")
+nome2 = input("Digite o nome da segunda pessoa: ")
+print(f"{nome1} e {nome2} formam uma grande dupla!")
+
+#Exercicio 10
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cor = input("Digite sua cor favorita: ")
+cidade = input("Digite sua cidade: ")
+
+print("\n--- Cadastro ---")
+print("Nome:", nome)
+print("Idade:", idade)
+print("Cor favorita:", cor)
+print("Cidade:", cidade)
